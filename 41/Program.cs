a033fd2010ba99e000cf04a870427275cc91fdd8
st202222 <zarina.kochisova@gmail.com>
2023-01-20 23:52:01 -0500

    added files

---
+++ 41/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+Write("Введите числа через пробел: ");
+//пользователь вводит числа, которые мы кладём в строку, преобразуем в int 
+int[] numbers = Array.ConvertAll(ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+WriteLine($"Вы ввели {CountPositive(numbers)} положительных чисел");
+
+//метод для поиска положительных чисел
+int CountPositive(int[] innum)
+{
+    int result = 0;
+    for(int i = 0; i < innum.Length; i++)
+    {
+        if(innum[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+
+}
+
+
---
+++ 43/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+Write("Введите число b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+//WriteLine(b1);
+Write("Введите число k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Write("Введите число b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Write("Введите число k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = GetX(b1, k1, b2, k2);
+double y = GetY(b1, k1, b2, k2);
+WriteLine($"Прямые пересекаются в точках X равной {x} и Y равной {y}");
+
+double GetX(double beone, double keyone, double betwo, double keytwo)
+{
+    double resultx = (betwo - beone) / (keyone - keytwo);
+    return resultx;
+}
+
+double GetY(double beone, double keyone, double betwo, double keytwo)
+{
+    double resulty = ((betwo * keyone)- (beone * keytwo)) / (keyone - keytwo);
+    return resulty;
+}
